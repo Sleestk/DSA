2fa8cc364cd9b20818f7fa31b7fb93f2bb161934
Sleestk <151932726+Sleestk@users.noreply.github.com>
2026-01-21 10:52:00 -0700

    Time: 1 ms (18.45%) | Memory: 19.4 MB (9.47%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,22 +4,30 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # create a dummy node (edge case)
         dummy = ListNode()
         tail = dummy
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
+        # Go through each list
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                # update our 1st pointer
+                l1 = l1.next
             else:
-                tail.next = list2
-                list2 = list2.next
+                tail.next = l2
+                # update our 2nd pointer
+                l2 = l2.next
+            # update our tail pointer
             tail = tail.next
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next =list2
+
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
+
         return dummy.next
-        
-        
+
+# Time Complexity: O(n + m) because the while loop gets one node from either l1 or l2; no node is revisted
+# Space Complexity: O(1) because no new nodes are created, only constant memory is used
