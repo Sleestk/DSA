33f8e05144d39af59d381f684e78d32a455ee219
Sleestk <151932726+Sleestk@users.noreply.github.com>
2026-01-21 12:19:50 -0700

    Time: 0 ms (100.00%) | Memory: 20.6 MB (11.61%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # iterate through the array 
-        left, right = 0, 1 
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-# Time complexity O(n^2) using two nested loops
-# Space complexity O(1) since we are iterating in place
+        # if O(n) Time complexity then we need to creat a hashmap
+        hashSet = {} # Gives the index : value
+        for i in range(len(nums)): # Gives our indicies
+            currNum = nums[i]
+            comp = target - currNum
+            if comp in hashSet:
+                return [hashSet[comp], i]
+            hashSet[currNum] = i
+# Time Complexity: O(n) because hash map for constant time lookups
+# Space Complexity: O(n) because space to store previous seen numbers
