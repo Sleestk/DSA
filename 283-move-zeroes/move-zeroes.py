8fa2819b95b94493f35caf880802f2d5e83a1ce4
Sleestk <151932726+Sleestk@users.noreply.github.com>
2026-01-23 11:27:18 -0700

    Time: 7 ms (42.33%) | Memory: 20.6 MB (20.36%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,16 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Quicksort Algo
-        # Left pointer starting a the index 0
+        # left pointer initalized at zero
         l = 0
-        # right pointer that will go through the nums array
+        # right pointer iterates through the entire array
         for r in range(len(nums)):
+            # if right pointer value is non zero
             if nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
-                # update left point 
                 l += 1
-        # return the number
         return nums
-# Time complexity O(n) since it iterates through the array once
-# Space complexity O(1) since the operation is done in place without extra memory
